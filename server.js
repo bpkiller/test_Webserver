bc40ad7aa16adec270864689345d81b1eca6eb2d
Pongsathorn C <bpkiller45@gmail.com>
2016-12-20 22:16:15 +0700

    Init repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 80;
+var middleware = {
+  requireAuthentication: function(req,res,next) {
+    console.log('Private route hit!');
+    next();
+  },
+  logger: function(req,res,next) {
+    var date = new Date().toString();
+    console.log('Request: '+date+' '+req.method+' '+req.originalUrl);
+    next();
+  }
+};
+app.use(middleware.logger);
+// app.use(middleware.requireAuthentication);
+// app.get('/',function(req,res) {
+//   res.send('Hello Express!');
+// });
+
+app.get('/about',middleware.requireAuthentication,function(req,res) {
+  res.send('<h1>About Us</h1>');
+});
+app.use(express.static(__dirname+'/public'));
+app.listen(port,function() {
+  console.log('Express server started! port:'+port);
+});
